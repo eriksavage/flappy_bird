96b75ae614147832eaf81fa05d70072d7bf24357
erik savage <erik.k.savage@gmail.com>
2023-09-09 14:32:39 -0700

    add file structures, implement pipe spawning

--- Assets/Scripts/SpawnPipeSet.cs
+++ Assets/Scripts/SpawnPipeSet.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class SlideLeft : MonoBehaviour
+public class SpawnPipeSet : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject pipeSet;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Instantiate(pipeSet, gameObject.transform);
+        }
     }
 }
