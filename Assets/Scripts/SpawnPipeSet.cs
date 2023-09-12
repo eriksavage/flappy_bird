c5c24aba023667b575f4f130d33f21b2c1fbe6ab
erik savage <erik.k.savage@gmail.com>
2023-09-11 18:01:46 -0700

    refactored pipeset prefab origin to match world origin

--- Assets/Scripts/SpawnPipeSet.cs
+++ Assets/Scripts/SpawnPipeSet.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class SpawnPipeSet : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject pipeSet;
+    public float yOffset;
     void Start()
     {
 
@@ -15,9 +17,14 @@ public class SpawnPipeSet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float highestSpawnPoint = transform.position.y + yOffset;
+        float lowestSpawnPoint = transform.position.y - yOffset;
+        var spawnPosition = new Vector3(gameObject.transform.position.x, Random.Range(lowestSpawnPoint, highestSpawnPoint), 0);
+
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            Instantiate(pipeSet, gameObject.transform);
+            Instantiate(pipeSet, spawnPosition, gameObject.transform.rotation);
+            Debug.Log($"PipeSet Spawned At: {spawnPosition}");
         }
     }
 }
