1e93d285ec7c1e03de546b2e5df309eb21ea7a7f
erik savage <erik.k.savage@gmail.com>
2023-09-20 09:15:10 -0700

    final commit, switching to Godot

--- Assets/Scripts/SpawnPipeSet.cs
+++ Assets/Scripts/SpawnPipeSet.cs
@@ -9,6 +9,7 @@ public class SpawnPipeSet : MonoBehaviour
     // Start is called before the first frame update
     public GameObject pipeSet;
     public float yOffset;
+    public float spawnInterval;
     void Start()
     {
 
@@ -17,14 +18,19 @@ public class SpawnPipeSet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SpawnPipes();
+        }
+    }
+
+    private void SpawnPipes()
+    {
         float highestSpawnPoint = transform.position.y + yOffset;
         float lowestSpawnPoint = transform.position.y - yOffset;
         var spawnPosition = new Vector3(gameObject.transform.position.x, Random.Range(lowestSpawnPoint, highestSpawnPoint), 0);
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Instantiate(pipeSet, spawnPosition, gameObject.transform.rotation);
-            Debug.Log($"PipeSet Spawned At: {spawnPosition}");
-        }
+        Instantiate(pipeSet, spawnPosition, gameObject.transform.rotation);
+        Debug.Log($"PipeSet Spawned At: {spawnPosition}");
     }
 }
